216fb8b4d127249839d6e8792fb6beaf6436c328
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2024-11-15 05:34:18 +0800

    Join other table for data display

--- admin/function/management/display_patient.php
+++ admin/function/management/display_patient.php
@@ -1,17 +1,26 @@
 <?php
 include 'db.php';
 
-$sql = "SELECT id, patient_name, room_number, date_of_birth, admit_date, admit_time FROM patient_management ORDER BY id DESC";
+$sql = "SELECT pm.id, pm.patient_name, pm.room_number, pm.date_of_birth, pm.admit_date, pm.admit_time, 
+               iv.device_id, iv.liter, iv.percent 
+        FROM patient_management pm 
+        LEFT JOIN iv_data iv ON pm.device_id = iv.device_id 
+        ORDER BY pm.id DESC";
+
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
+if ($result && $result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        // Use print_r for debugging (remove it after testing)
         echo "<tr>
                 <td>{$row['patient_name']}</td>
                 <td>{$row['room_number']}</td>
                 <td>{$row['date_of_birth']}</td>
                 <td>{$row['admit_date']}</td>
                 <td>{$row['admit_time']}</td>
+                <td>" . (!empty($row['device_id']) ? $row['device_id'] : "N/A") . "</td>
+                <td>{$row['liter']}</td>
+                <td>{$row['percent']}</td>
                 <td>
                     <div class='dropdown'>
                         <button class='dropdown-button' onclick='toggleDropdown(event)'>Select &#11206;</button>
@@ -22,7 +31,10 @@ if ($result->num_rows > 0) {
                                 '{$row['room_number']}', 
                                 '{$row['date_of_birth']}', 
                                 '{$row['admit_date']}', 
-                                '{$row['admit_time']}'
+                                '{$row['admit_time']}',
+                                '{$row['device_id']}',
+                                '{$row['liter']}',
+                                '{$row['percent']}'
                             )\">Edit</a>
                             <a href='function/management/delete.php?id={$row['id']}' onclick='return confirm(\"Are you sure you want to delete this record PERMANENTLY?\");'>Delete</a>
                         </div>
@@ -31,7 +43,8 @@ if ($result->num_rows > 0) {
               </tr>";
     }
 } else {
-    echo "<tr><td colspan='6' style='color: red;'>No records found.</td></tr>";
+    echo "<tr><td colspan='9' style='color: red;'>No records found.</td></tr>";
 }
+
 $conn->close();
 ?>
