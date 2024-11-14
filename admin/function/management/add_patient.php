e5c30411404269c7a3c7201bd43d51d75aab2fbf
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2024-11-15 05:31:53 +0800

    Add device_id parameter

--- admin/function/management/add_patient.php
+++ admin/function/management/add_patient.php
@@ -1,6 +1,7 @@
 <?php
 include '../../db.php';
 
+$device_id = $conn->real_escape_string($_POST['device_id']);
 $patient_name = $conn->real_escape_string($_POST['name']);
 $room_number = $conn->real_escape_string($_POST['room']);
 $date_of_birth = $conn->real_escape_string($_POST['birth']);
@@ -8,8 +9,8 @@ $admit_date = $conn->real_escape_string($_POST['admit_date']);
 $admit_time = $conn->real_escape_string($_POST['admit_time']);
 
 // Insert the data into the patients table
-$sql = "INSERT INTO patient_management (patient_name, room_number, date_of_birth, admit_date, admit_time) 
-        VALUES ('$patient_name', '$room_number', '$date_of_birth', '$admit_date', '$admit_time')";
+$sql = "INSERT INTO patient_management (patient_name, room_number, date_of_birth, admit_date, admit_time, device_id) 
+        VALUES ('$patient_name', '$room_number', '$date_of_birth', '$admit_date', '$admit_time', '$device_id')";
 
 if ($conn->query($sql) === TRUE) {
     header("Location: ../../management.php"); 
