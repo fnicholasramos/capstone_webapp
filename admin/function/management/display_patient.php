2850e0dabee3a7659150e497ebd484339aec9303
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2024-11-18 04:22:58 +0800

    Remove real-time data

--- admin/function/management/display_patient.php
+++ admin/function/management/display_patient.php
@@ -1,40 +1,29 @@
 <?php
 include 'db.php';
 
-$sql = "SELECT pm.id, pm.patient_name, pm.room_number, pm.date_of_birth, pm.admit_date, pm.admit_time, 
-               iv.device_id, iv.liter, iv.percent 
-        FROM patient_management pm 
-        LEFT JOIN iv_data iv ON pm.device_id = iv.device_id 
-        ORDER BY pm.id DESC";
+$sql = "SELECT id, patient_name, date_of_birth, admit_date, admit_time
+        FROM patient_management
+        ORDER BY id DESC";
 
 $result = $conn->query($sql);
 
 if ($result && $result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        // Use print_r for debugging (remove it after testing)
         echo "<tr>
                 <td>{$row['patient_name']}</td>
-                <td>{$row['room_number']}</td>
                 <td>{$row['date_of_birth']}</td>
                 <td>{$row['admit_date']}</td>
                 <td>{$row['admit_time']}</td>
-                <td>" . (!empty($row['device_id']) ? $row['device_id'] : "N/A") . "</td>
-                <td>{$row['liter']}</td>
-                <td>{$row['percent']}</td>
-                <td>
-                    <div class='dropdown'>
+                <td style='text-align:center;'>
+                    <div class='dropdown' style='text-align:left;'>
                         <button class='dropdown-button' onclick='toggleDropdown(event)'>Select &#11206;</button>
                         <div class='dropdown-menu'>
                             <a href='#' onclick=\"openEditModal(
                                 '{$row['id']}',
                                 '{$row['patient_name']}', 
-                                '{$row['room_number']}', 
                                 '{$row['date_of_birth']}', 
                                 '{$row['admit_date']}', 
-                                '{$row['admit_time']}',
-                                '{$row['device_id']}',
-                                '{$row['liter']}',
-                                '{$row['percent']}'
+                                '{$row['admit_time']}'
                             )\">Edit</a>
                             <a href='function/management/delete.php?id={$row['id']}' onclick='return confirm(\"Are you sure you want to delete this record PERMANENTLY?\");'>Delete</a>
                         </div>
