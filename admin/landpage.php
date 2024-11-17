7163057c962f1c7bb8260f6e4708476c83899cd2
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2024-11-18 04:28:08 +0800

    Change landpage design and add acknowledgment

--- admin/landpage.php
+++ admin/landpage.php
@@ -14,53 +14,57 @@
     <div class="container">
         <div class="title">
             <div>
-                <img src="../assets/images/logos-removebg-preview (1).png" height="70px">
+                <a href="https://github.com/fnicholasramos/capstone_webapp"><img src="../assets/images/github.png" height="50px"></a>
+                <a href="https://www.facebook.com/or1equals1"><img src="../assets/images/fb-blue.png" height="50px"></a>
+                
             </div>
             
             <div class="important">
-                <p class="onm">OSPITAL NG MAYNILA MEDICAL CENTER</p>
-                <p class="location">Quirino Avenue cor. Mabini Street, Malate</p>
-                <p class="email">ommc@manila.gov.ph</p>
+                <p class="onm">Report technical problem</p>
+                <p class="location">GitHub Repository</p>
+                <p class="email">pfrancisnicholas@gmail.com</p>
             </div>
             
         </div>    
 
         <div class="overview">
             <div class="text">
-            Ospital ng Maynila Medical Center is a non-profit tertiary, general and training hospital. It was established on January 31, 1969 by the government of the City of Manila.
+            This study, conducted by BSIT students of Pateros Technological College, focuses on developing HealthGuard, an innovative IV Bag Monitoring and Alert System designed to improve healthcare efficiency and patient safety.
             </div>
         </div>
 
         <div class="box">
-            <div class="vision">
-                <h2>VISION</h2>
-                <p class="message">Provision of the highest quality medical service in all disciplines at the lowest possible cost</p>
+            <div class="client">
+                <div class="text-two">
+                The target respondents for this study are the registered nurses and IT staff of the Jose R. Reyes Memorial Medical Center (JRRMC), whose insights and feedback are vital for evaluating and improving the system.
+                </div>
             </div>
 
-            <div class="mission">
-                <h2>MISSION</h2>
-                <p class="message">To provide quality medical care to all Manilans regardless of social status, race and creed, made free to all indigent residents</p>
-            </div>
-
-            <div class="objectives">
-                <h2>OBJECTIVES</h2>
-                <p class="message">
-                    Patient Care: To provide city residents quality medical care at the lowest possible cost to the  city of Manila
-                    Education and Training: To provide a program of graduate and undergraduate training for medical, nursing and paramedical disciplines
-                    Public Health: To serve as the medical center of an integrated community health program
-                    Research: To provide and maintain an atmosphere of scientific excellence un order to structuralize and provide research activities.</p>
+            <div class="obj">
+                <h3 class="research">-- Research Objectives --</h3>
+                    <div class="list">
+                       <ul>
+                            <li>Develop a prototype with key features for IV monitoring and patient management, ensuring data privacy.</li>
+                            <li>Evaluate the system using surveys, descriptive analysis, and ISO-25010 standards.</li>
+                            <li>Create a user-friendly interface for nurses and doctors, incorporating feedback.</li>
+                            <li>Enable real-time IV monitoring to reduce nursing workload and improve efficiency.</li>
+                            <li>Complete development, testing, and evaluation within the project timeline.</li>
+                       </ul>
+                       
+                    </div>
             </div>
         </div>
 
-        <div class="geoLocation">
-            <div class="locate">
-                <h3>Location</h3>
-
-                <a href="https://www.google.com/maps/place/Bagong+Ospital+ng+Maynila/@14.5644829,120.9861882,18.5z/data=!4m10!1m2!2m1!1sOspital+ng+maynila!3m6!1s0x3397cbd5e886ae79:0x4aacbd0edd3ff9e6!8m2!3d14.5644279!4d120.9869911!15sChJPc3BpdGFsIG5nIG1heW5pbGGSARNnb3Zlcm5tZW50X2hvc3BpdGFs4AEA!16zL20vMGducXoy?entry=ttu&g_ep=EgoyMDI0MTEwNi4wIKXMDSoASAFQAw%3D%3D" target="_blank">
-                    <img src="../assets/images/location.png" alt="maps" class="location_img">
-                </a>
+        <div class="ack">
+            <div class="acknowledgment">
+                <h2 class="ack-title">Acknowledgement</h2>
             </div>
             
+            <div class="ack-message">
+                <p class="ack-para">
+                This research was made possible by the guidance of Dean Reagan B. Ricafort, ENGR., the support of Jose R. Reyes Memorial Medical Center, and the mentorship of Pateros Technological College faculty and staff.
+                </p>
+            </div>
         </div>
 
     </div>
