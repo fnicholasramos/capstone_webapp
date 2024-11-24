ea7e68b20114fec0dc773572518661f13e4bf2f5
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2024-11-24 22:54:03 +0800

    Database: 2024/11/25 (Defense day)

--- capstone.sql
+++ capstone.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 20, 2024 at 11:35 PM
+-- Generation Time: Nov 24, 2024 at 03:52 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -43,12 +43,8 @@ CREATE TABLE `discharge` (
 --
 
 INSERT INTO `discharge` (`id`, `patient_name`, `iv_fluid`, `admission_date`, `admission_time`, `discharge_date`, `discharge_time`, `ivf_no`) VALUES
-(1, 'Edsel S. Nasol', 'Saline', '2024-11-10', '11:21pm', '2024-11-10', '11:23 PM', 1),
-(3, 'Ramon G. Gemaguim', 'Saline', '2024-11-10', '11:22pm', '2024-11-10', '11:36 PM', 2),
-(12, 'Test Patient', 'Elixir', '2024-11-20', '11:40 PM', '2024-11-20', '08:23 PM', 1),
-(13, 'Test Patient', 'Elixir', '2024-11-20', '11:40 PM', '2024-11-20', '10:03 PM', 1),
-(14, 'Test Pasyente', 'Saline', '2024-11-21', '5:21 am', '2024-11-21', '05:56 AM', 5),
-(15, 'Francis Nicholas', 'Saline', '2024-11-20', '10:34pm', '2024-11-21', '05:56 AM', 1);
+(19, 'Ramon G. Gemaguim', 'Saline', '2024-11-10', '11:22pm', '2024-11-24', '06:50 PM', 2),
+(20, 'Francis Nicholas P. Ramos', 'Saline', '2024-11-24', '10:13pm ', '2024-11-24', '10:15 PM', 1);
 
 -- --------------------------------------------------------
 
@@ -82,11 +78,7 @@ CREATE TABLE `doc_orders` (
 --
 
 INSERT INTO `doc_orders` (`id`, `patient_name`, `room_number`, `iv_fluid_name`, `volume`, `flow_rate`, `answer`, `drop_factor`, `minutes`, `drip_rate_answer`, `incorp`, `ivf_no`, `date_started`, `time_started`, `date_consumed`, `time_consumed`, `nurse`, `device_id`) VALUES
-(1, 'Edsel S. Nasol', '0001', 'Saline', 1000, 9, '111', '20', '60', '37.00', '', '1', '2024-11-10', '11:21pm', '2024-11-10', '12:21pm', 'Demi Lovato', 'null'),
-(2, 'Ramon G. Gemaguim', '0002', 'Saline', 1000, 5, '200', '20', '120', '33.33', '', '2', '2024-11-10', '11:22pm', '2024-11-10', '2:00am', 'Justin Bieber', 'null'),
-(5, 'Edrian Salasalan', '0010', 'RX Saline', 1000, 8, '125', '15', '7', '267.86', '', '5', '2024-11-17', '10:50pm', '2024-11-20', '10:50pm', 'Jonathan Velasquez', 'null'),
-(7, 'Juan Dela Cruz', '0006', 'Saline', 1000, 8, '125', '20', '60', '41.67', '', '5', '2024-11-17', '11:40 PM', '2024-11-30', '11:59 pm', 'Justin Bieber', 'null'),
-(52, 'Francis Nicholas P. Ramos', '0005', 'Dark Elixir', 1000, 9, '111', '20', '50', '44.40', '', '1', '2024-11-21', '10:34pm', '2024-11-21', '10:34pm', 'Lady Gaga', 'pt0001');
+(1, 'Juan Dela Cruz', '0001', 'Saline', 1000, 9, '111', '20', '60', '37.00', '', '1', '2024-11-10', '11:21pm', '2024-11-10', '12:21pm', 'Sophia Nicole Macam', 'pt0001');
 
 -- --------------------------------------------------------
 
@@ -107,7 +99,7 @@ CREATE TABLE `iv_data` (
 --
 
 INSERT INTO `iv_data` (`device_id`, `liter`, `percent`, `flow_rate_sensor`, `time`) VALUES
-('pt0001', 0, 0, 0, '2024-11-20 21:55:10'),
+('pt0001', 0, 0, 0, '2024-11-24 14:30:11'),
 ('pt0002', 0, 0, 0, '2024-11-17 17:25:24');
 
 -- --------------------------------------------------------
@@ -132,10 +124,6 @@ CREATE TABLE `patient_management` (
 --
 
 INSERT INTO `patient_management` (`id`, `patient_name`, `room_number`, `date_of_birth`, `admit_date`, `admit_time`, `actions`, `device_id`) VALUES
-(45, 'Francis Nicholas P. Ramos', '0001', '2000/01/01', '2000/01/01', '5:58 pm', '', '0'),
-(46, 'Edrian Salasalan', '0002', '2000/01/01', '2000/01/01', '5:58 pm', '', '0'),
-(47, 'Edsel S. Nasol', '0003', '2000/01/01', '2000/01/01', '5:59 pm', '', '0'),
-(48, 'Ramon G. Gemaguim', '0004', '2000/01/01', '2000/01/01', '5:59 pm', '', '0'),
 (49, 'Juan Dela Cruz', '2000/', '2000/01/01', '2024/11/18', '2:40 am', '', '0');
 
 -- --------------------------------------------------------
@@ -158,7 +146,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `email`, `token`, `expires_at`) VALUES
-(1, 'admin', '$2y$10$QuexlpREzqCXegm.Xfc6MuSAQwleNMVwmpyHlGDnZc31HjVbmx1WG', 'ramosnicholas75@gmail.com', '215b2b0b8ec5600011cd0b9cd97f33b4b52a864d55b59bc2329ae274ea3922f6', '2024-11-17 13:59:03');
+(1, 'admin', '$2y$10$QuexlpREzqCXegm.Xfc6MuSAQwleNMVwmpyHlGDnZc31HjVbmx1WG', 'ramosnicholas75@gmail.com', '215b2b0b8ec5600011cd0b9cd97f33b4b52a864d55b59bc2329ae274ea3922f6', '2024-11-17 13:59:03'),
+(12, 'ramon', '$2y$10$WAi7HWYEAYFcSf7Sz57zJ.pYtcJaV0hSL.mvLJfe5m1kY9uyBimVa', 'gemaguimr@gmail.com', '', NULL),
+(13, 'edsel', '$2y$10$caGqQKoS5QfhvpPeuWV7YuXwAAQ2EXnJknDIV9rlhaMpYPPO6tZa6', 'edselnasol2@gmail.com', '', NULL);
 
 --
 -- Indexes for dumped tables
@@ -202,25 +192,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `discharge`
 --
 ALTER TABLE `discharge`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `doc_orders`
 --
 ALTER TABLE `doc_orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
 -- AUTO_INCREMENT for table `patient_management`
 --
 ALTER TABLE `patient_management`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
